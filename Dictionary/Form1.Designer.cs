44064acdaaabc53fe9994f0bf21ec7fdb77a46c6
fellanaura <thufellanaura@gmail.com>
2020-12-23 15:53:03 +0700

    dictionary

---
+++ Dictionary/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelWord = new System.Windows.Forms.Label();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt12 = new System.Windows.Forms.TextBox();
+            this.txt11 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelWaktu = new System.Windows.Forms.Label();
+            this.labelTgl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.BackColor = System.Drawing.Color.Plum;
+            this.labelWord.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWord.Location = new System.Drawing.Point(12, 173);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(55, 19);
+            this.labelWord.TabIndex = 0;
+            this.labelWord.Text = "Word :";
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.BackColor = System.Drawing.Color.White;
+            this.textBoxWord.Location = new System.Drawing.Point(81, 173);
+            this.textBoxWord.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(219, 20);
+            this.textBoxWord.TabIndex = 1;
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnFind.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFind.Font = new System.Drawing.Font("Tekton Pro Ext", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFind.Location = new System.Drawing.Point(355, 173);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(88, 25);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "FIND";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(399, 226);
+            this.txt4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(123, 20);
+            this.txt4.TabIndex = 9;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(270, 226);
+            this.txt3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(123, 20);
+            this.txt3.TabIndex = 8;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(141, 226);
+            this.txt2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(123, 20);
+            this.txt2.TabIndex = 7;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 226);
+            this.txt1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(123, 20);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(141, 264);
+            this.txt6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(123, 20);
+            this.txt6.TabIndex = 27;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(12, 264);
+            this.txt5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(123, 20);
+            this.txt5.TabIndex = 26;
+            // 
+            // txt8
+            // 
+            this.txt8.Location = new System.Drawing.Point(399, 264);
+            this.txt8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(123, 20);
+            this.txt8.TabIndex = 29;
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(270, 264);
+            this.txt7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(123, 20);
+            this.txt7.TabIndex = 28;
+            // 
+            // txt12
+            // 
+            this.txt12.Location = new System.Drawing.Point(399, 304);
+            this.txt12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt12.Name = "txt12";
+            this.txt12.Size = new System.Drawing.Size(123, 20);
+            this.txt12.TabIndex = 33;
+            // 
+            // txt11
+            // 
+            this.txt11.Location = new System.Drawing.Point(270, 304);
+            this.txt11.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt11.Name = "txt11";
+            this.txt11.Size = new System.Drawing.Size(123, 20);
+            this.txt11.TabIndex = 32;
+            // 
+            // txt10
+            // 
+            this.txt10.Location = new System.Drawing.Point(141, 304);
+            this.txt10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(123, 20);
+            this.txt10.TabIndex = 31;
+            // 
+            // txt9
+            // 
+            this.txt9.Location = new System.Drawing.Point(12, 304);
+            this.txt9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(123, 20);
+            this.txt9.TabIndex = 30;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Rosewood Std Regular", 38.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(-4, -2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(544, 150);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "ENGLISH DICTIONARY";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // labelWaktu
+            // 
+            this.labelWaktu.AutoSize = true;
+            this.labelWaktu.BackColor = System.Drawing.Color.Plum;
+            this.labelWaktu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.labelWaktu.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWaktu.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelWaktu.Location = new System.Drawing.Point(25, 9);
+            this.labelWaktu.Name = "labelWaktu";
+            this.labelWaktu.Size = new System.Drawing.Size(97, 24);
+            this.labelWaktu.TabIndex = 35;
+            this.labelWaktu.Text = "Waktu";
+            // 
+            // labelTgl
+            // 
+            this.labelTgl.AutoSize = true;
+            this.labelTgl.BackColor = System.Drawing.Color.PowderBlue;
+            this.labelTgl.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTgl.ForeColor = System.Drawing.Color.Firebrick;
+            this.labelTgl.Location = new System.Drawing.Point(327, 9);
+            this.labelTgl.Name = "labelTgl";
+            this.labelTgl.Size = new System.Drawing.Size(100, 25);
+            this.labelTgl.TabIndex = 36;
+            this.labelTgl.Text = "Tanggal";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Tekton Pro Ext", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClear.Location = new System.Drawing.Point(447, 172);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 26);
+            this.btnClear.TabIndex = 37;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(533, 351);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.labelTgl);
+            this.Controls.Add(this.labelWaktu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt12);
+            this.Controls.Add(this.txt11);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.labelWord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "English Dictionary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt12;
+        private System.Windows.Forms.TextBox txt11;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelWaktu;
+        private System.Windows.Forms.Label labelTgl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Dictionary/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Dictionary
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string API = "dict.1.1.20161113T050014Z.6b0875d1a4748f03.5fa57e579db9cb75107915ea230a9d8d3321526f";
+            string Word = textBoxWord.Text;
+            string url = string.Format("https://dictionary.yandex.net/api/v1/dicservice/lookup?key={0}&lang=en-en&text={1}", API, Word);
+            xmlDoc.Load(url);
+
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("/DicResult/def/tr");
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Arti1", typeof(string));
+            dt.Columns.Add("Arti2", typeof(string));
+            dt.Columns.Add("Arti3", typeof(string));
+            dt.Columns.Add("Arti4", typeof(string));
+            dt.Columns.Add("Arti5", typeof(string));
+            dt.Columns.Add("Arti6", typeof(string));
+            dt.Columns.Add("Arti7", typeof(string));
+            dt.Columns.Add("Arti8", typeof(string));
+            dt.Columns.Add("Arti9", typeof(string));
+            dt.Columns.Add("Arti10", typeof(string));
+            dt.Columns.Add("Arti11", typeof(string));
+            dt.Columns.Add("Arti12", typeof(string));            
+            foreach (XmlNode item in nodes)
+            {
+                DataRow dr = dt.NewRow();
+                dr[0] = item["text"].InnerText;
+                dr[1] = item["text"].InnerText;
+                dr[2] = item["text"].InnerText;
+                dr[3] = item["text"].InnerText;
+                dr[4] = item["text"].InnerText;
+                dr[5] = item["text"].InnerText;
+                dr[6] = item["text"].InnerText;                
+                dt.Rows.Add(dr);
+            }
+
+                try
+                {
+                    string d1 = dt.Rows[0][1].ToString();
+                    txt1.Text = d1.ToString();
+                    string d2 = dt.Rows[1][1].ToString();
+                    txt2.Text = d2.ToString();
+                    string d3 = dt.Rows[2][1].ToString();
+                    txt3.Text = d3.ToString();
+                    string d4 = dt.Rows[3][1].ToString();
+                    txt4.Text = d4.ToString();
+                    string d5 = dt.Rows[4][1].ToString();
+                    txt5.Text = d5.ToString();
+                    string d6 = dt.Rows[5][1].ToString();
+                    txt6.Text = d6.ToString();
+                    string d7 = dt.Rows[6][1].ToString();
+                    txt7.Text = d7.ToString();
+                    string d8 = dt.Rows[7][1].ToString();
+                    txt8.Text = d8.ToString();
+                    string d9 = dt.Rows[8][1].ToString();
+                    txt9.Text = d9.ToString();
+                    string d10 = dt.Rows[9][1].ToString();
+                    txt10.Text = d10.ToString();
+                    string d11 = dt.Rows[10][1].ToString();
+                    txt11.Text = d11.ToString();
+                    string d12 = dt.Rows[11][1].ToString();
+                    txt12.Text = d12.ToString();
+                }
+                catch (Exception ex)
+                {
+                    
+                }            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelWaktu.Text = DateTime.Now.ToLongTimeString();
+            labelTgl.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxWord.Text = "";
+            txt1.Text = "";
+            txt2.Text = "";
+            txt3.Text = "";
+            txt4.Text = "";
+            txt5.Text = "";
+            txt6.Text = "";
+            txt7.Text = "";
+            txt8.Text = "";
+            txt9.Text = "";
+            txt10.Text = "";
+            txt11.Text = "";
+            txt12.Text = "";            
+        }
+    }
+}
